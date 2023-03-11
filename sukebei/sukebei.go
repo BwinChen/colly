dfaf3eb46b0f5c068122f7d15188fca7e7277f84
BwinChen <erotic129@126.com>
2023-03-11 17:04:37 +0800

    增加截止时间功能

--- sukebei/sukebei.go
+++ sukebei/sukebei.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
 	"log"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
-
-func Save(r *colly.Response) {
-	if strings.Contains(r.Request.URL.String(), ".torrent") {
-		if err := r.Save(fmt.Sprintf("./%s.torrent", r.Ctx.Get("InfoHash"))); err != nil {
-			log.Println(err)
-		}
-	}
-}
 
 func ParseList(b *colly.HTMLElement) {
 	// 列表
 	b.ForEach("tr", func(_ int, tr *colly.HTMLElement) {
+		t := tr.ChildAttr("td:nth-child(5)", "data-timestamp")
+		if t != "" {
+			t1, err := strconv.ParseInt(t, 10, 64)
+			if err != nil {
+				return
+			}
+			if deadline > t1 {
+				// 早于截止时间，停止爬取
+				os.Exit(0)
+			}
+		}
 		tr.ForEach("td", func(i int, td *colly.HTMLElement) {
 			if i == 1 {
 				if err := td.Request.Visit(td.ChildAttr("a", "href")); err != nil {
@@ -40,11 +46,11 @@ func ParseList(b *colly.HTMLElement) {
 func ParseInfo(div *colly.HTMLElement) {
 	title := div.ChildText("div > div:nth-child(5) > div.panel-heading > h3")
 	if title != "" {
-		fmt.Println("title", title)
+		fmt.Println("title:", title)
 	}
 	infoHash := div.ChildText("kbd")
 	if infoHash != "" {
-		fmt.Println("infoHash", infoHash)
+		fmt.Println("infoHash:", infoHash)
 	}
 	div.ForEach("div.panel-footer > a", func(i int, a *colly.HTMLElement) {
 		if i == 0 {
@@ -55,10 +61,31 @@ func ParseInfo(div *colly.HTMLElement) {
 			}
 		}
 		if i == 1 {
-			fmt.Println("magnet: ", a.Attr("href"))
+			fmt.Println("magnet:", a.Attr("href"))
 		}
 	})
 	div.ForEach(".torrent-file-list i.fa-file", func(_ int, i *colly.HTMLElement) {
 		fmt.Println("file:", i.DOM.Parent().Text())
 	})
 }
+
+func Save(r *colly.Response) {
+	if strings.Contains(r.Request.URL.String(), ".torrent") {
+		if err := r.Save(fmt.Sprintf("./torrents/%s.torrent", r.Ctx.Get("InfoHash"))); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func Deadline(duration string) int64 {
+	n := time.Now()
+	d, err := time.ParseDuration(duration)
+	if err != nil {
+		log.Fatal(err)
+	}
+	n = n.Add(d)
+	t2 := n.Unix()
+	return t2
+}
+
+var deadline = Deadline("-14h")
